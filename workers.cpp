b8438f24a94e646af8a84d96eec01325b0e35d21
Eric B <ericbieszczad@gmail.com>
2023-02-07 14:06:12 +0100

    Moved worker function out into its own function

--- workers.cpp
+++ workers.cpp
@@ -1,22 +1,37 @@
 #include "workers.h"
+#include <atomic>
+#include <mutex>
 #include <list>
 #include <thread>
 #include <iostream>
-#include <mutex>
 #include <functional>
 
 std::mutex cout_lock;
+std::mutex task_lock;
 
-void worker(int i) {
-    const std::lock_guard<std::mutex> lock(cout_lock);
-    std::cout << "Hello from thread " << i << "\n";
+void worker(int i, std::atomic<bool> &running, std::list<std::function<void()>> &task_list) {
+    {
+        const std::lock_guard<std::mutex> lock(cout_lock);
+        std::cout << "Hello from thread " << i << "\n";
+    }
+    while (running) {
+        const std::lock_guard<std::mutex> lock(task_lock);
+        /*
+        std::function<void()> task;
+        if (!task_list.empty()) {
+            task = task_list.front();
+            task_list.pop_front();
+        }
+        task();
+        */
+    }
 }
 
 Workers::Workers(int worker_count) {
+    running = true;
     for (int i = 0; i < worker_count; i++) {
-        worker_threads.emplace_back(std::thread(worker, i));
+        worker_threads.emplace_back(std::thread(worker, i, std::ref(running), std::ref(task_list)));
     }
-
     return;
 }
 
@@ -26,6 +41,7 @@ void Workers::post(std::function<void()> task) {
 
 void Workers::join() {
     std::cout << "Joining all threads!\n";
+    running = false;
     for (std::thread &t : worker_threads) {
         t.join();
     }
--- workers.h
+++ workers.h
@@ -1,11 +1,13 @@
 #include <list>
 #include <thread>
 #include <functional>
+#include <atomic>
 
 class Workers {
     private:
         std::list<std::thread> worker_threads;
         std::list<std::function<void()>> task_list;
+        std::atomic<bool> running;
 
     public:
         Workers(int worker_count);
